0fe705d68ccefaabbe9e7f5f6a2cc23923e1b480
gumsanmarip <gumsanmarip@gmail.com>
2022-01-21 23:25:28 -0500

    work

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,41 @@
-
+import {
+    FETCH_START,
+    FETCH_SUCCESS,
+    ADD_SMURF,
+    SET_ERROR_TEXT } from './../actions';
 export const initialState = {
+    errorText:"",
+    isLoading: false,
+    smurfs:[]
 }
 
-const reducer = ()=>{
+const reducer = (state= initialState, action)=>{
+    switch(action.type) {
+        case(FETCH_START):
+            return({
+                ...state,
+                isLoading: true
+            });
+        case(FETCH_SUCCESS):
+            return({
+                ...state,
+                isLoading: false,
+                smurfs: action.payload
+            });
+        case(ADD_SMURF):
+            return({
+                ...state,
+                smurfs:[...state.smurfs, action.payload]
+            });
+        case(SET_ERROR_TEXT):
+            return({
+                ...state,
+                isLoading: false,
+                errorText:action.payload
+            });
+        default:
+            return(state);
+    }
 }
 
 //**************DO NOT EDIT ANY CODE BEYOND THIS POINT**************//
